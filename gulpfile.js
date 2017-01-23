576f1a1b60c1e225fb5f0b706d756eb634d1120b
Yujin Jeong <jjiny1127@gmail.com>
2017-01-23 22:21:07 +0900

    travis test

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 var gulp = require('gulp'),
     qunit = require('gulp-qunit');
 
-gulp.task('default', function() {
-    return gulp.src('./test/*.html')
-        .pipe(qunit());
+gulp.task('test', function() {
+    return gulp.src('./test/*.html').pipe(qunit());
 });
