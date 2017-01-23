cf5328bb0d690e2170a577929a261b1199d511ff
Yujin Jeong <jjiny1127@gmail.com>
2017-01-23 22:23:16 +0900

    update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 var gulp = require('gulp'),
     qunit = require('gulp-qunit');
 
+gulp.task('default', function() {
+});
+
 gulp.task('test', function() {
     return gulp.src('./test/*.html').pipe(qunit());
 });
