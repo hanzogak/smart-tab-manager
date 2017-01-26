8a970d68702bf837dca1f5db9f09878732cf70dc
persona0220 <persona0220@naver.com>
2017-01-26 20:29:06 +0900

    Added Sujin's name (#5)
    * Added Sujin's name
    
    * fix sujin's ID at ".pullapprove.yml" file.
    
    * Travis gulp test name change (#6)
    
    * Initial commit
    
    * Initial commit
    
    * Added a new line at the bottom of the file.
    
    * Followed the code style of Google and  used jQuery
    
    * first travis setting with qunit
    
    * jquery not yet works (#2)
    
    * first travis setting with qunit (#4)
    
    * travis test
    
    * update gulpfile
    
    * update travis.yml
    
    * update travis.yml
    
    * -
    
    * -
    
    * -
    
    * jquery adding
    
    * jquery add
    
    * -
    
    * -

--- src/js/manager.js
+++ src/js/manager.js
@@ -1,32 +1,6 @@
 /**
  * Create a new tab for the url that use entered.
  */
-function openNewTab() {
-  // Get the url from manager.html that user entered.
-  let url = document.querySelector('#url').value;
-
-  // Create a new tab with the url.
-  chrome.tabs.create({ "url": url, "selected": true });
-}
-
-function changeOption() {
-  let option = document.querySelector('#option');
-
-  switch(document.querySelector('#command').value) {
-    case 'search':
-      option.append("<option>-url</option>");
-      option.append("<option>-title</option>");
-      break;
-    case 'order':
-      option.append("<option>-time</option>");
-      option.append("<option>-name</option>");
-      break;
-  }
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-  document.body.querySelector('#btn-open-new-tab')
-    .addEventListener('click', openNewTab);
-  document.body.querySelector('#command')
-    .addEventListener('change', changeOption);
+$(function() {
+  //for travis test
 });
