832c27f7da03c2d1875a8f391b53eb020988802e
Yujin Jeong <jjiny1127@gmail.com>
2017-01-26 19:27:56 +0900

    -

--- src/js/manager.js
+++ src/js/manager.js
@@ -1,30 +1,6 @@
 /**
  * Create a new tab for the url that use entered.
  */
-function openNewTab() {
-  // Get the url from manager.html that user entered.
-  var url = $('#url').value();
-
-  // Create a new tab with the url.
-  chrome.tabs.create({ "url": url, "selected": true });
-}
-
-function changeOption() {
-  var option = $('#option');
-
-  switch(document.querySelector('#command').value()) {
-    case 'search':
-      option.append("<option>-url</option>");
-      option.append("<option>-title</option>");
-      break;
-    case 'order':
-      option.append("<option>-time</option>");
-      option.append("<option>-name</option>");
-      break;
-  }
-}
-
 $(function() {
-    $('#btn-open-new-tab').click(openNewTab);
-    $('#command').change(changeOption);
+  //for travis test
 });
