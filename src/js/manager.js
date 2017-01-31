6fb4537a99531ac5927880a518d4646fece37374
persona0220 <persona0220@naver.com>
2017-01-31 22:15:54 +0900

    change javascript to jquery

--- src/js/manager.js
+++ src/js/manager.js
@@ -1,147 +1,123 @@
 /**
  * Create a new tab for the url that use entered.
  */
-function openNewTab() {
-  // Get the url from manager.html that user entered.
-  var url = document.querySelector('#url').value;
-
-  if (!(url.toString().substr(0, 8) === "https://"
-      || url.toString().substr(0, 7) === "http://")) {
-    url = "http://" + url;
-  }
-
-  // Create a new tab with the url.
-  chrome.tabs.create({"url": url, "selected": true});
-}
-
 function changeOption() {
-  var option = document.querySelector('#option');
-
-  var op = new Option();
-  for(var i=1; i<option.options.length; i++){
-    option.options[i] = null;
-  }
+  var option = $('#option');
+  option.empty();
 
   switch ($('#command').val()) {
+    case 'close':
+    case 'window':
     case 'search':
-      option.options.length = 0;
-      option.add(new Option('-url', '-url'));
-      option.add(new Option('-title', '-title'));
+      option.append(new Option('-url', 'url'));
+      option.append(new Option('-title', 'title'));
       break;
     case 'order':
-      option.options.length = 0;
-      option.add(new Option('-time', '-time'));
-      option.add(new Option('-name', '-name'));
+      option.append(new Option('-time', 'time'));
+      option.append(new Option('-name', 'name'));
       break;
     case 'open':
-      option.options.length = 0;
-      option.add(new Option('-url', '-url'));
-      option.add(new Option('-saved', '-saved'));
+      option.append(new Option('-url', 'url'));
+      option.append(new Option('-saved', 'saved'));
       break;
-    case 'close':
-      option.options.length = 0;
-      option.add(new Option('-url', '-url'));
-      option.add(new Option('-title', '-title'));
-      break;
-    case 'window':
-      option.options.length = 0;
-      option.add(new Option('-url', '-url'));
-      option.add(new Option('-title', '-title'));
   }
 }
 
-function commandSubmit(){
-  var option = document.querySelector('#option');
-  switch(document.querySelector('#command').value){
+function commandSubmit() {
+
+  $('#error_message').text('');
+
+  var command = $('#command').val();
+  var option = $('#option').val();
+  var keyword = $('#keyword').val();
+
+  switch (command) {
     case 'search':
+      handleSearch(option, keyword);
       break;
 
     case 'order':
       break;
 
     case 'open':
-      var url = document.querySelector('#keyword').value;
-
-      if(option.value === '-url'){
-        if (!(url.toString().substr(0, 8) === "https://"
-            || url.toString().substr(0, 7) === "http://")) {
-          url = "http://" + url;
-        }
-        chrome.tabs.create({"url": url, "selected": true});
-      }
-      else if(option.value === '-saved'){
-        //todo need 'save' function
-      }
+      handleOpen(option, keyword);
       break;
 
     case 'close':
-      if(document.querySelector('#keyword').value.length == 0){
-        chrome.tabs.query({"currentWindow": true}, function (tabs) {
-          for(var i=0; i<tabs.length-1; i++){
-            chrome.tabs.remove(tabs[i].id);
-          }
-        });
-      }
-      else {
-
-        var keyword = document.querySelector('#keyword').value;
-        if (option.value === '-url') {
-          chrome.tabs.query({"currentWindow": true}, function (tabs) {
-            for(var i=0; i<tabs.length; i++){
-              if(tabs[i].url.indexOf(keyword) > -1){
-                chrome.tabs.remove(tabs[i].id);
-              }
-            }
-          });
-        }
-        else if (option.value === '-title') {
-          chrome.tabs.query({"currentWindow": true}, function (tabs) {
-            for(var i=0; i<tabs.length; i++){
-              if(tabs[i].title.indexOf(keyword) > -1){
-                chrome.tabs.remove(tabs[i].id);
-              }
-            }
-          });
-        }
-      }
+      handleClose(option, keyword);
       break;
 
     case 'window':
-      var keyword = document.querySelector('#keyword').value;
-      var selectedTabs = [];
-      if(option.value === '-url'){
-        chrome.tabs.query({"currentWindow": true}, function(tabs){
-          for(var i = 0; i<tabs.length; i++) {
-            if(tabs[i].url.indexOf(keyword) > -1){
-              selectedTabs.push(tabs[i].id);
-            }
-          }
-          if(selectedTabs.length != 0){
-              chrome.windows.create({"tabId":selectedTabs[0]}, function(window){
-              chrome.tabs.move(selectedTabs, {"windowId":window.id, "index":-1});
-            })
-          }
+      handleWindow(option, keyword);
+      break;
+  }
+}
+function handleOpen(option, keyword) {
+  if (option === 'url') {
+    if (!(keyword.substr(0, 8) === 'https://'
+      || keyword.substr(0, 7) === 'http://')) {
+      keyword = 'http://' + keyword;
+    }
+    chrome.tabs.create({"url": keyword, "selected": true});
+  } else if (option === '-saved') {
+    //todo need 'save' function
+  }
+}
+
+function handleClose(option, keyword) {
+  if (option === 'url') {
+    chrome.tabs.query({"currentWindow": true}, function (tabs) {
+      for (var i = 0; i < tabs.length; i++) {
+        if (tabs[i].url.indexOf(keyword) > -1) {
+          chrome.tabs.remove(tabs[i].id);
+        }
+      }
+    });
+  }
+  else if (option === 'title') {
+    chrome.tabs.query({"currentWindow": true}, function (tabs) {
+      for (var i = 0; i < tabs.length; i++) {
+        if (tabs[i].title.indexOf(keyword) > -1) {
+          chrome.tabs.remove(tabs[i].id);
+        }
+      }
+    });
+  }
+}
+
+function handleWindow(option, keyword){
+  var selectedTabs = [];
+  if (option === 'url') {
+    chrome.tabs.query({"currentWindow": true}, function (tabs) {
+      for (var i = 0; i < tabs.length; i++) {
+        if (tabs[i].url.indexOf(keyword) > -1) {
+          selectedTabs.push(tabs[i].id);
+        }
+      }
+      if (selectedTabs.length != 0) {
+        chrome.windows.create({"tabId": selectedTabs[0]}, function (window) {
+          chrome.tabs.move(selectedTabs, {"windowId": window.id, "index": -1});
         })
       }
-      else if(option.value === '-title'){
-        chrome.tabs.query({"currentWindow": true}, function(tabs){
-          for(var i = 0; i<tabs.length; i++) {
-            if(tabs[i].title.indexOf(keyword) > -1){
-              selectedTabs.push(tabs[i].id);
-            }
-          }
-          if(selectedTabs.length != 0){
-            chrome.windows.create({"tabId":selectedTabs[0]}, function(window){
-              chrome.tabs.move(selectedTabs, {"windowId":window.id, "index":-1});
-            })
-          }
+    })
+  }
+  else if (option === 'title') {
+    chrome.tabs.query({"currentWindow": true}, function (tabs) {
+      for (var i = 0; i < tabs.length; i++) {
+        if (tabs[i].title.indexOf(keyword) > -1) {
+          selectedTabs.push(tabs[i].id);
+        }
+      }
+      if (selectedTabs.length != 0) {
+        chrome.windows.create({"tabId": selectedTabs[0]}, function (window) {
+          chrome.tabs.move(selectedTabs, {"windowId": window.id, "index": -1});
         })
       }
+    })
   }
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-    $('#btn-open-new-tab').click(openNewTab);
-    $('#command').change(changeOption);
-    $('#command-submit').click(commandSubmit);
+  $('#command').change(changeOption);
+  $('#command-submit').click(commandSubmit);
 });
