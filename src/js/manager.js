2b65a0c4fde58fc7296f707175cfc2ad774206ad
YuJin Jeong <jjiny1127@gmail.com>
2017-01-31 22:22:55 +0900

    Develop Search (#8)
    * main popup design & search one tab
    * let to var
    * use constant variable - options

--- src/js/manager.js
+++ src/js/manager.js
@@ -1,6 +1,89 @@
-/**
- * Create a new tab for the url that use entered.
+var options = {
+  'search': ['url', 'title'],
+  'open': ['url', 'title'],
+  'close': ['url', 'title'],
+  'order': ['time', 'name']
+};
+
+/*
+ * function that start with dom starting
  */
-$(function() {
-  //for travis test
+$(function () {
+  $('#command').change(changeOption).change();
+
+  // click submit button
+  $('#command-submit').click(commandSubmit);
+
+  // enter keyboard in keyword input
+  $('#keyword').keydown(function (e) {
+    if (e.keyCode == 13) {
+      commandSubmit();
+    }
+  });
 });
+
+/*
+ * function for change option for command selection
+ */
+function changeOption() {
+  var option = $('#option');
+  option.empty();
+
+  var command = $('#command').val();
+
+  for (var i = 0; i < options[command].length; i++) {
+    option.append(new Option('-' + options[command][i], options[command][i]));
+  }
+}
+
+/*
+ * function for command submit
+ */
+function commandSubmit() {
+  $('#error-message').text('');
+
+  var command = $('#command').val();
+  var option = $('#option').val();
+  var keyword = $('#keyword').val();
+
+  switch (command) {
+    case 'search':
+      handleSearch(option, keyword);
+      break;
+  }
+}
+
+/*
+ * function for search
+ */
+function handleSearch(option, keyword) {
+  if (!keyword) {
+    $('#error-message').text('input any keyword');
+    return;
+  }
+
+  if (option == options.search[0]) {
+    chrome.tabs.query({currentWindow: true}, function (tabList) {
+      for (var i = 0; i < tabList.length; i++) {
+        if (tabList[i].url.includes(keyword)) {
+          chrome.tabs.highlight({'tabs': i});
+          return;
+        }
+      }
+
+      $('#error-message').text('no matched tabs');
+    });
+  }
+  else if (option == options.search[1]) {
+    chrome.tabs.query({currentWindow: true}, function (tabList) {
+      for (var i = 0; i < tabList.length; i++) {
+        if (tabList[i].title.includes(keyword)) {
+          chrome.tabs.highlight({'tabs': i});
+          return;
+        }
+      }
+
+      $('#error-message').text('no matched tabs');
+    });
+  }
+}
--- test/test.js
+++ test/test.js
@@ -1,10 +1,10 @@
-QUnit.test("first test", function(assert) {
-    var actual = 5 - 4;
+QUnit.test("first test", function (assert) {
+  var actual = 5 - 4;
 
-    assert.ok(true, "passes because true is true");
-    //ok is boolean assertion that passes if the first argument is truthy.
-    assert.equal(actual, 1, "passes because 1 == 1");
-    //actual == expected
-    assert.notEqual(actual, 0, "passes because 1 != 0");
-    //actual != expected
+  assert.ok(true, "passes because true is true");
+  //ok is boolean assertion that passes if the first argument is truthy.
+  assert.equal(actual, 1, "passes because 1 == 1");
+  //actual == expected
+  assert.notEqual(actual, 0, "passes because 1 != 0");
+  //actual != expected
 });
