d7877910c0e13b737cd2357bc6c0e4e62d4baa18
ttochi <jjiny1127@gmail.com>
2017-02-07 01:25:54 +0900

    click event on preview page

--- src/js/preview.js
+++ src/js/preview.js
@@ -9,7 +9,35 @@ $(function () {
       tabInfo.find('.title').text(tabs[i].title);
       tabInfo.find('.url').text(tabs[i].url);
 
+      if(tabs[i].highlighted) {
+        tabInfo.addClass('active');
+      }
+
       tabList.append(tabInfo);
+
+      clickListener(tabInfo, tabs[i].id);
     }
   });
+
+  function clickListener(tabInfo, tabId) {
+    tabInfo.click(function(e) {
+      if(e.target.className == 'delete') {
+        chrome.tabs.remove(tabId, function () {
+          tabInfo.remove();
+        });
+      } else {
+        var index = $('.tab').index(tabInfo);
+        chrome.tabs.highlight({'tabs': index});
+      }
+    });
+  }
+
+  // TODO
+  function captureTabScreen(tabId, screenImg) {
+    chrome.tabs.onActivated.addListener(function () {
+      chrome.tabs.captureVisibleTab(function (capturedUrl) {
+        screenImg.attr('src', capturedUrl);
+      })
+    });
+  }
 });
