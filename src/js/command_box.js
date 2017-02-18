53e475cc8f8e48cc7a5348c82257c2f8d801e37b
YuJin Jeong <jjiny1127@gmail.com>
2017-02-18 17:28:01 +0900

    command box placeholder autocomplete (#15)
    * doing preview before capture
    
    * click event on preview page
    
    * search duplicated
    
    * merge ing
    
    * merge & seperate
    
    * merge css
    
    * implement autocomplete. add two files for JQueryUI.
    
    * show/hide keywordbox when command/option change
    
    * delete save -view(alert all savelist, delete savelist)
    
    * dashboard
    
    * prepare keyboard shortcut develop
    
    * solve conflict between 'show/hide keyword box' and 'keyword autocomplete'. Alsom, localstorage format changed
    
    * organize js file
    
    * command box
    
    * implement autocomplete in command_box. need to be overlapped
    
    * preview command in commandbox

--- src/js/command_box.js
+++ src/js/command_box.js
@@ -1,5 +1,35 @@
+var commandList = [
+  'search',
+  'order',
+  'open',
+  'close',
+  'window',
+  'save',
+  'suspend',
+  'preview',
+  'merge'
+];
+//
+var CONST_URL = '-url';
+var CONST_TITLE = '-title';
+var CONST_SAVED = '-saved';
+var CONST_ALL = '-all';
+var CONST_TIME = '-time';
+
+var optionList = {
+  'search': [CONST_URL, CONST_TITLE],
+  'open': [CONST_URL, CONST_SAVED],
+  'close': [CONST_ALL, CONST_URL, CONST_TITLE],
+  'order': [CONST_TIME, CONST_TITLE],
+  'window': [CONST_ALL, CONST_URL, CONST_TITLE],
+  'save': [CONST_ALL, CONST_URL, CONST_TITLE],
+  'preview': [CONST_ALL],
+  'merge': [CONST_URL, CONST_TITLE],
+  'suspend': ['-older',CONST_ALL, CONST_URL, CONST_TITLE] // how to indicate all tabs?
+};
+
 $.ajax({
-  url: 'chrome-extension://maihpbfhkfmblcnhnmobbgikkckbfhhk/src/html/command_box.html',
+  url: 'chrome-extension://' + chrome.runtime.id + '/src/html/command_box.html',
   type: 'GET',
   success: function (data) {
     $('body').append($(data)).css('overflow-y', 'hidden');
@@ -10,5 +40,62 @@ $.ajax({
         $('body').css('overflow-y', 'scroll');
       }
     });
+
+    previewCommand();
+
   }, dataType: 'HTML'
 });
+
+function previewCommand() {
+  var inputDiv = $('#smart-tab-manager-input');
+  var previewDiv = $('#smart-tab-manager-preview');
+  var word ='';
+
+  inputDiv.keyup(function(e){
+    var val = $(this).val();
+    var stage = 0;
+    var pre = '';
+
+    //set stage
+    while(val.indexOf(' ') > 0 && stage < 2){
+      pre += val.slice(0, val.indexOf(' ') + 1);
+      val = val.slice(val.indexOf(' ') + 1,val.length);
+      stage++;
+    }
+
+    //empty case
+    if(val == ''){
+      previewDiv.text('');
+      return;
+    }
+
+    //auto complete
+    if(e.which === 39 && word != ''){  //(->)
+      e.preventDefault();
+      inputDiv.val(word + " ");
+      previewDiv.text(word + " ");
+      return;
+    }
+
+    var src;
+    if(stage == 0) {
+      src = commandList;
+    } else if (stage == 1) {
+      src = optionList[pre.slice(0, pre.length-1)];
+    } else if (stage == 2 && pre == 'open -saved '){
+      //TODO : LocalStorage 불러오는 법을 찾아야함
+      return;
+    }
+
+    for(var i = 0; i< src.length; i++){
+      if(src[i].indexOf(val) === 0){
+        word = src[i];
+        break;
+      }
+      word = '';
+    }
+
+    word = pre + word;
+    previewDiv.text(word);
+  });
+}
