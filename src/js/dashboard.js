b1c57073afd0334730c17284b6f481b0563f303a
ttochi <jjiny1127@gmail.com>
2017-02-18 21:59:34 +0900

    saved list call in dashboard

--- src/js/dashboard.js
+++ src/js/dashboard.js
@@ -1,56 +1,74 @@
-function updateTabList() {
-  var tabList = $('#tab-list');
+function updatePreviewTabList() {
+  // update current preview list only when preview list is active
+  if($('.list-name.preview').hasClass('active')) {
+    var tabList = $('#tab-list');
 
-  chrome.tabs.query({"currentWindow": true}, function (tabs) {
-    tabList.empty();
+    chrome.tabs.query({"currentWindow": true}, function (tabs) {
+      tabList.empty();
 
-    for (var i = 0; i < tabs.length; i++) {
-      var newTab = createTabDiv(tabs[i]);
-      tabList.append(newTab);
-    }
-  });
+      for (var i = 0; i < tabs.length; i++) {
+        var newTab = createTabDiv(tabs[i]);
+        tabList.append(newTab);
+      }
+    });
+  }
 }
 
 chrome.tabs.onCreated.addListener(function () {
-  updateTabList();
+  updatePreviewTabList();
 });
 
 chrome.tabs.onHighlighted.addListener(function () {
-  updateTabList();
+  updatePreviewTabList();
 });
 
 chrome.tabs.onRemoved.addListener(function () {
-  updateTabList();
+  updatePreviewTabList();
 });
 
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo) {
   if (changeInfo.status == 'complete')
-    updateTabList();
+    updatePreviewTabList();
 });
 
 chrome.tabs.onMoved.addListener(function () {
-  updateTabList();
+  updatePreviewTabList();
 });
 
 $(function () {
   var tabList = $('#tab-list');
+  var previewList = $('.list-name.preview');
 
   sortTabDiv(tabList);
 
-  for (var i = 0; i < localStorage.length; i++) {
-    var listName = $('.list-name').clone().removeClass('preview');
-    listName.text(localStorage.key(i));
+  previewList.click(function() {
+    $(this).addClass('active');
+    updatePreviewTabList();
+  });
 
-    listName.click(function (e) {
+  // call saved lists from local storage
+  for (var i = 0; i < localStorage.length; i++) {
+    // append list name to left side
+    var listName = $('.list-name').last().clone().removeClass('preview');
+    listName.text(localStorage.key(i));
+    $('#left-side').append(listName);
+
+    // list name click event
+    listName.click(function() {
+      previewList.removeClass('active');
+      $(this).addClass('active');
       tabList.empty();
-      var savedTab = JSON.parse(localStorage.getItem(listName.text()));
-      console.log('savedTab');
-      for (i in savedTab) {
-        var newTab = createTabDiv(savedTab[i]);
+
+      var savedListName = $(this).text();
+
+      var mergedTabs = JSON.parse(localStorage.getItem(savedListName));
+
+      sortStorageTabDiv(tabList, savedListName);
+
+      for (var i in mergedTabs) {
+        var newTab = createStorageTabDiv(mergedTabs[i], savedListName);
         tabList.append(newTab);
       }
     });
-
-    $('#left-side').append(listName);
   }
 });
